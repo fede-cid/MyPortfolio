3e0bebcfe15ad9f2eb1a5de9991ae6b8aec392c9
fede-cid <fede.cid.96@gmail.com>
2023-03-13 23:55:04 -0300

    update

--- MyPortfolio/frontend/vite.config.js
+++ MyPortfolio/frontend/vite.config.js
@@ -4,10 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'public/build',
+    chunkSizeWarningLimit: 500,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          router: ['react-router-dom'],
+        },
+      },
+    },
+  },
   server: {
     open: true,
     port: 3000,
     host: true
   }
 })
-
